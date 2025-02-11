429442f7546e141583351c49dffa721bafe16112
heroes75 <123456789+odin@users.noreply.github.com>
2025-02-11 18:23:50 +0000

    add button toggle read or not read

--- script.js
+++ script.js
@@ -3,36 +3,45 @@ const myLibrary = [
         name: "Titanic",
         author: "Cameroon",
         pages: 135,
-        read: false,
+        reads: false,
         getBook: function() {
             return `<div class="card">
             <button id="delete-book" class="delete-book"><iconify-icon icon="material-symbols:close" width="24" height="24"  style="color: red"></iconify-icon></button>
                 <p>${this.name}</p><p>${this.author}</p><p>${this.pages} pages</p><p>${this.read ? "read" : "not read"}</p>
             </div>`
+        },
+        toggleRead: function() {
+            this.reads = !this.reads
         }
     },
     {
         name: "Monte Christo",
         author: "Alexandre Dumas",
         pages: 135,
-        read: true,
+        reads: true,
         getBook: function() {
             return `<div class="card">
             <button id="delete-book" class="delete-book"><iconify-icon icon="material-symbols:close" width="24" height="24"  style="color: red"></iconify-icon></button>
                 <p>${this.name}</p><p>${this.author}</p><p>${this.pages} pages</p><p>${this.read ? "read" : "not read"}</p>
             </div>`
+        },
+        toggleRead: function() {
+            this.reads = !this.reads
         }
     },
     {
         name: "Monte Christo",
         author: "Alexandre Dumas",
         pages: 135,
-        read: true,
+        reads: true,
         getBook: function() {
             return `<div class="card">
             <button id="delete-book" class="delete-book"><iconify-icon icon="material-symbols:close" width="24" height="24"  style="color: red"></iconify-icon></button>
                 <p>${this.name}</p><p>${this.author}</p><p>${this.pages} pages</p><p>${this.read ? "read" : "not read"}</p>
             </div>`
+        },
+        toggleRead: function() {
+            this.reads = !this.reads
         }
     }
 ];
@@ -64,6 +73,11 @@ function Book(name, author, pages, booleen) {
     }
 }
 
+Book.prototype.toggleRead = function() {
+    this.reads = !this.reads;
+    
+}
+
 /*deleteBook.forEach(button => {
     button.addEventListener("click", (e) => {
         e.preventDefault();
@@ -82,22 +96,30 @@ function displayBook() {
     //const bookInformation = myLibrary.map(el => el.getBook()).join("");
     //bookContainer.innerHTML = bookInformation;
     bookContainer.textContent = ""
-    myLibrary.forEach(element => {
+    myLibrary.forEach((element, i) => {
         const card = document.createElement("div");
-        const deleteButton = document.createElement("button")
+        const deleteButton = document.createElement("button");
+        const buttonToggle = document.createElement("button");
+        buttonToggle.textContent = element.reads ? "read" : "not Read";
+        buttonToggle.style.backgroundColor = element.reads ? "green" : "red";
         card.appendChild(deleteButton);
         deleteButton.setAttribute("id", "delete-book");
-        deleteButton.innerHTML = '<button id="delete-book" class="delete-book"><iconify-icon icon="material-symbols:close" width="24" height="24"  style="color: red"></iconify-icon></button>'
+        deleteButton.style.padding = "0"
+        deleteButton.style.border = "0"
+        deleteButton.innerHTML = '<iconify-icon icon="material-symbols:close" style="margin: 0; border: 0;" width="24" height="24"  style="color: red"></iconify-icon>'
         card.classList.add("card");
         bookContainer.appendChild(card);
         const p1 = document.createElement("p");
         card.appendChild(p1);
+        card.appendChild(buttonToggle);
         p1.textContent = element.getBook();
         deleteButton.onclick = (e) => {
             e.preventDefault();
             e.target.style.backgroundColor = "red";
-            console.log(e.target)
+            myLibrary.splice(i, 1);
+            displayBook();
         }
+        buttonToggle.onclick = element.toggleRead();
     });
 }
 addButton.addEventListener("click", (e) => {
